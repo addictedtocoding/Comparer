fc9cb056d3f5ae5cb2946c6ed7884ac145252e6e
addictedtocoding <agupta10000@yahoo.com>
2017-07-22 22:50:30 -0700

    Create comparer.java

---
+++ comparer.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package comparer;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author xanatosducrion
+ */
+public class Comparer extends Application {
+    
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
+        
+        Scene scene = new Scene(root);
+        
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+}
+
+
+//woodshop 101
+
+//mold for notches- see traditional japanese woodworking
+
+//stuff I want: pure c r- engraving, sk/noose/kn erg 
+
+//stuff I want to get into: carpentry, metalworking: first projects- engraved bookshelf with locking notches (ie. no nails), copper bowl, copper ring, 1, chm wristbands, arm warmers
+//p sl that imitates the soil type in a) b) c)
+//arche, 'ging, ph
+
+//st ttg/pups, r c, hema
